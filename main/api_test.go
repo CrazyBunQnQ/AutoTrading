dd0fb83db6a8db2cf083b3de44febc8bb2560cdd
CrazyBunQnQ <bjj15811278699>
2019-07-17 23:31:45 +0800

    Add the get Binance KLine interface test

--- main/api_test.go
+++ main/api_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestDepth(t *testing.T) {
-	//log.Println(api.BianDepth("BTCUSDT", 5))
-	//log.Println(api.GetMarketDepth("btcusdt", "step0"))
+	log.Println(api.BianDepth("BTCUSDT", 5))
+	log.Println(api.GetMarketDepth("btcusdt", "step0"))
 
-	log.Println(api.GetBianKLine("BTCUSDT", models.BianDay, 500, 0, 0))
+	log.Println(api.GetBianKLine("BTCUSDT", models.BianDay, 10, 0, 0))
 	log.Println(api.GetHuobiKLine("btcusdt", "15min", 10))
 }
