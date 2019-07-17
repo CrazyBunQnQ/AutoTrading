b36c0750950c7996e5113d064c958df5b0a3ad0b
CrazyBunQnQ <bjj15811278699>
2019-07-17 21:19:02 +0800

    Add Binance models.

---
+++ models/BinanceModels.go
@@ -0,0 +1,395 @@
+package models
+
+import "time"
+
+type BianDepth struct {
+	LastUpdateID int        `json:"lastUpdateId"`
+	Bids         [][]string `json:"bids"` // 买盘, [price(成交价), amount(成交量)], 按price降序排列
+	Asks         [][]string `json:"asks"` // 卖盘, [price(成交价), amount(成交量)], 按price升序排列
+}
+
+// OrderBook represents Bids and Asks.
+type OrderBook struct {
+	LastUpdateID int `json:"lastUpdateId"`
+	Bids         []*Order
+	Asks         []*Order
+}
+
+type DepthEvent struct {
+	WSEvent
+	UpdateID int
+	OrderBook
+}
+
+// Order represents single order information.
+type Order struct {
+	Price    float64
+	Quantity float64
+}
+
+// OrderBookRequest represents OrderBook request data.
+type OrderBookRequest struct {
+	Symbol string
+	Limit  int
+}
+
+// AggTrade represents aggregated trade.
+type AggTrade struct {
+	ID             int
+	Price          float64
+	Quantity       float64
+	FirstTradeID   int
+	LastTradeID    int
+	Timestamp      time.Time
+	BuyerMaker     bool
+	BestPriceMatch bool
+}
+
+type AggTradeEvent struct {
+	WSEvent
+	AggTrade
+}
+
+// AggTradesRequest represents AggTrades request data.
+type AggTradesRequest struct {
+	Symbol    string
+	FromID    int64
+	StartTime int64
+	EndTime   int64
+	Limit     int
+}
+
+// KlinesRequest represents Klines request data.
+type KlinesRequest struct {
+	Symbol    string
+	Interval  Interval
+	Limit     int
+	StartTime int64
+	EndTime   int64
+}
+
+// Kline represents single Kline information.
+type Kline struct {
+	OpenTime                 time.Time
+	Open                     float64
+	High                     float64
+	Low                      float64
+	Close                    float64
+	Volume                   float64
+	CloseTime                time.Time
+	QuoteAssetVolume         float64
+	NumberOfTrades           int
+	TakerBuyBaseAssetVolume  float64
+	TakerBuyQuoteAssetVolume float64
+}
+
+type KlineEvent struct {
+	WSEvent
+	Interval     Interval
+	FirstTradeID int64
+	LastTradeID  int64
+	Final        bool
+	Kline
+}
+
+// TickerRequest represents Ticker request data.
+type TickerRequest struct {
+	Symbol string
+}
+
+// Ticker24 represents data for 24hr ticker.
+type Ticker24 struct {
+	PriceChange        float64
+	PriceChangePercent float64
+	WeightedAvgPrice   float64
+	PrevClosePrice     float64
+	LastPrice          float64
+	BidPrice           float64
+	AskPrice           float64
+	OpenPrice          float64
+	HighPrice          float64
+	LowPrice           float64
+	Volume             float64
+	OpenTime           time.Time
+	CloseTime          time.Time
+	FirstID            int
+	LastID             int
+	Count              int
+}
+
+// PriceTicker represents ticker data for price.
+type PriceTicker struct {
+	Symbol string
+	Price  float64
+}
+
+// BookTicker represents book ticker data.
+type BookTicker struct {
+	Symbol   string
+	BidPrice float64
+	BidQty   float64
+	AskPrice float64
+	AskQty   float64
+}
+
+// NewOrderRequest represents NewOrder request data.
+type NewOrderRequest struct {
+	Symbol           string
+	Side             OrderSide
+	Type             OrderType
+	TimeInForce      TimeInForce
+	Quantity         float64
+	Price            float64
+	NewClientOrderID string
+	StopPrice        float64
+	IcebergQty       float64
+	Timestamp        time.Time
+}
+
+// ProcessedOrder represents data from processed order.
+type ProcessedOrder struct {
+	Symbol        string
+	OrderID       int64
+	ClientOrderID string
+	TransactTime  time.Time
+}
+
+// QueryOrderRequest represents QueryOrder request data.
+type QueryOrderRequest struct {
+	Symbol            string
+	OrderID           int64
+	OrigClientOrderID string
+	RecvWindow        time.Duration
+	Timestamp         time.Time
+}
+
+// ExecutedOrder represents data about executed order.
+type ExecutedOrder struct {
+	Symbol        string
+	OrderID       int
+	ClientOrderID string
+	Price         float64
+	OrigQty       float64
+	ExecutedQty   float64
+	Status        OrderStatus
+	TimeInForce   TimeInForce
+	Type          OrderType
+	Side          OrderSide
+	StopPrice     float64
+	IcebergQty    float64
+	Time          time.Time
+}
+
+// CancelOrderRequest represents CancelOrder request data.
+type CancelOrderRequest struct {
+	Symbol            string
+	OrderID           int64
+	OrigClientOrderID string
+	NewClientOrderID  string
+	RecvWindow        time.Duration
+	Timestamp         time.Time
+}
+
+// CanceledOrder represents data about canceled order.
+type CanceledOrder struct {
+	Symbol            string
+	OrigClientOrderID string
+	OrderID           int64
+	ClientOrderID     string
+}
+
+// OpenOrdersRequest represents OpenOrders request data.
+type OpenOrdersRequest struct {
+	Symbol     string
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// AllOrdersRequest represents AllOrders request data.
+type AllOrdersRequest struct {
+	Symbol     string
+	OrderID    int64
+	Limit      int
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// AccountRequest represents Account request data.
+type AccountRequest struct {
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// Account represents user's account information.
+type Account struct {
+	MakerCommision  int64
+	TakerCommision  int64
+	BuyerCommision  int64
+	SellerCommision int64
+	CanTrade        bool
+	CanWithdraw     bool
+	CanDeposit      bool
+	Balances        []*HuobiBalance
+}
+
+type AccountEvent struct {
+	WSEvent
+	Account
+}
+
+// HuobiBalance groups balance-related information.
+type BianBalance struct {
+	Asset  string
+	Free   float64
+	Locked float64
+}
+
+// MyTradesRequest represents MyTrades request data.
+type MyTradesRequest struct {
+	Symbol     string
+	Limit      int
+	FromID     int64
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// Trade represents data about trade.
+type Trade struct {
+	ID              int64
+	Price           float64
+	Qty             float64
+	Commission      float64
+	CommissionAsset string
+	Time            time.Time
+	IsBuyer         bool
+	IsMaker         bool
+	IsBestMatch     bool
+}
+
+// WithdrawRequest represents Withdraw request data.
+type WithdrawRequest struct {
+	Asset      string
+	Address    string
+	Amount     float64
+	Name       string
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// WithdrawResult represents Withdraw result.
+type WithdrawResult struct {
+	Success bool
+	Msg     string
+}
+
+// HistoryRequest represents history-related calls request data.
+type HistoryRequest struct {
+	Asset      string
+	Status     *int
+	StartTime  time.Time
+	EndTime    time.Time
+	RecvWindow time.Duration
+	Timestamp  time.Time
+}
+
+// Deposit represents Deposit data.
+type Deposit struct {
+	InsertTime time.Time
+	Amount     float64
+	Asset      string
+	Status     int
+}
+
+// Withdrawal represents withdrawal data.
+type Withdrawal struct {
+	Amount    float64
+	Address   string
+	TxID      string
+	Asset     string
+	ApplyTime time.Time
+	Status    int
+}
+
+// Stream represents stream information.
+//
+// Read web docs to get more information about using streams.
+type Stream struct {
+	ListenKey string
+}
+
+type WSEvent struct {
+	Type   string
+	Time   time.Time
+	Symbol string
+}
+
+type DepthWebsocketRequest struct {
+	Symbol string
+}
+
+type KlineWebsocketRequest struct {
+	Symbol   string
+	Interval Interval
+}
+
+type TradeWebsocketRequest struct {
+	Symbol string
+}
+
+type UserDataWebsocketRequest struct {
+	ListenKey string
+}
+
+// Interval represents interval enum.
+type Interval string
+
+var (
+	Minute         = Interval("1m")
+	ThreeMinutes   = Interval("3m")
+	FiveMinutes    = Interval("5m")
+	FifteenMinutes = Interval("15m")
+	ThirtyMinutes  = Interval("30m")
+	Hour           = Interval("1h")
+	TwoHours       = Interval("2h")
+	FourHours      = Interval("4h")
+	SixHours       = Interval("6h")
+	EightHours     = Interval("8h")
+	TwelveHours    = Interval("12h")
+	Day            = Interval("1d")
+	ThreeDays      = Interval("3d")
+	Week           = Interval("1w")
+	Month          = Interval("1M")
+)
+
+// TimeInForce represents timeInForce enum.
+type TimeInForce string
+
+var (
+	GTC = TimeInForce("GTC")
+	IOC = TimeInForce("IOC")
+)
+
+// OrderStatus represents order status enum.
+type OrderStatus string
+
+// OrderType represents order type enum.
+type OrderType string
+
+// OrderSide represents order side enum.
+type OrderSide string
+
+var (
+	StatusNew             = OrderStatus("NEW")
+	StatusPartiallyFilled = OrderStatus("PARTIALLY_FILLED")
+	StatusFilled          = OrderStatus("FILLED")
+	StatusCancelled       = OrderStatus("CANCELED")
+	StatusPendingCancel   = OrderStatus("PENDING_CANCEL")
+	StatusRejected        = OrderStatus("REJECTED")
+	StatusExpired         = OrderStatus("EXPIRED")
+
+	TypeLimit  = OrderType("LIMIT")
+	TypeMarket = OrderType("MARKET")
+
+	SideBuy  = OrderSide("BUY")
+	SideSell = OrderSide("SELL")
+)
