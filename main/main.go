6173698b2d1cbd6aa780c54c6399c4794b4ecb39
CrazyBunQnQ <baobao222222@qq.com>
2019-07-20 16:42:33 +0800

    Add an order api.

--- main/main.go
+++ main/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"AutoTrading/api"
-	"log"
+	"io"
+	"net/http"
 )
 
 func main() {
+
 	//bianPrice := api.BianTrades("BTCUSDT", "1").Get(0, "price").ToFloat64()
 	//log.Println("bian cur btc price: " + strconv.FormatFloat(bianPrice, 'f', -1, 64))
 	//huobiPrice := api.GetTrade("btcusdt", 1)
 	//log.Println("huobi cur btc price: " + strconv.FormatFloat(huobiPrice.Data[0].Data[0].Price, 'f', -1, 64))
 	//otcbtcPrice := api.OtcbtcTrades("btcusdt", "1").Get(0, "price").ToFloat64()
 	//log.Println("otcbtc cur btc price: " + strconv.FormatFloat(otcbtcPrice, 'f', -1, 64))
-	log.Println(api.BianOrderQuery("XRPUSDT", "", 207779114))
+	//log.Println(api.BianOrderQuery("XRPUSDT", "", 207779114))
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, api.BianOrderQuery("XRPUSDT", "", 207779114).Price)
 }
