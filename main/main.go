2c706fbcba8fedefae3d735dc30e2de91e07c93b
CrazyBunQnQ <baobao222222@qq.com>
2019-07-20 15:24:53 +0800

    There is no longer a "can't find profile" prompt, And the ability to package configuration files into binaries.

--- main/main.go
+++ main/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"AutoTrading/api"
+	"log"
+)
+
 func main() {
 	//bianPrice := api.BianTrades("BTCUSDT", "1").Get(0, "price").ToFloat64()
 	//log.Println("bian cur btc price: " + strconv.FormatFloat(bianPrice, 'f', -1, 64))
@@ -7,4 +12,5 @@ func main() {
 	//log.Println("huobi cur btc price: " + strconv.FormatFloat(huobiPrice.Data[0].Data[0].Price, 'f', -1, 64))
 	//otcbtcPrice := api.OtcbtcTrades("btcusdt", "1").Get(0, "price").ToFloat64()
 	//log.Println("otcbtc cur btc price: " + strconv.FormatFloat(otcbtcPrice, 'f', -1, 64))
+	log.Println(api.BianOrderQuery("XRPUSDT", "", 207779114))
 }
