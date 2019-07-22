8c51c62f670972f80f24a924870c8ea08a9445d3
CrazyBunQnQ <baobao222222@qq.com>
2019-07-22 12:13:09 +0800

    Add mysql database ORM support

--- dbhistory/account.sql
+++ dbhistory/account.sql
@@ -1,6 +1,21 @@
+create table autotrade.platform
+(
+    id          int auto_increment,
+    name        varchar(15)                                                     not null,
+    name_cn     varchar(10)                                                     null comment 'Chinese name',
+    create_time timestamp default CURRENT_TIMESTAMP                             not null,
+    update_time timestamp default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null,
+    constraint platform_pk
+        primary key (id),
+    constraint platform_name_uindex
+        unique (name)
+);
+
+
 create table autotrade.account
 (
-    id          varchar(10)                                                           not null,
+    id          int auto_increment,
+    platform    varchar(15)                                                           not null,
     usdt        decimal(18, 10) default 0.0000000000                                  not null comment 'usdt count',
     btc         decimal(18, 10) default 0.0000000000                                  not null comment 'btc count',
     eth         decimal(18, 10) default 0.0000000000                                  not null comment 'eth count',
@@ -10,7 +25,9 @@ create table autotrade.account
     create_time timestamp       default CURRENT_TIMESTAMP                             not null,
     update_time timestamp       default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null,
     constraint account_pk
-        primary key (id)
+        primary key (id),
+    constraint account_platform_uindex
+        unique (platform)
 )
     comment 'account status';
 
@@ -24,13 +41,15 @@ create table autotrade.strategy_low_buy_high_sell
     position_average    decimal(18, 10) default 0                                             not null comment 'Average purchase price',
     last_spend          decimal(18, 10) default 0                                             not null,
     target_profit_point double(4, 3)    default 1.05                                          not null comment 'Sell when the current price/average position price is higher than this value',
-    target_sell_price   decimal(18, 10) default 0                                             null,
+    target_sell_price   decimal(18, 10) default 0                                             not null,
     target_buy_point    double(4, 3)    default 0.92                                          not null comment 'Buy when the current price/average price is lower than this value',
     target_buy_price    decimal(18, 10) default 0                                             not null,
     month_average       decimal(18, 10) default 0                                             not null comment 'Average market price for a month (30 days)',
-    create_time         timestamp       default CURRENT_TIMESTAMP                             not null,
-    update_time         timestamp       default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null,
+    create_time         datetime        default CURRENT_TIMESTAMP                             not null,
+    update_time         datetime        default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null,
     constraint strategy_low_buy_high_sell_pk
-        primary key (id)
+        primary key (id),
+    constraint strategy_low_buy_high_sell_symbol_platform_uindex
+        unique (symbol, platform)
 );
 
--- models/DatabaseModels.go
+++ models/DatabaseModels.go
@@ -2,44 +2,54 @@ package models
 
 import (
 	"fmt"
+	"time"
 )
 
+type Platform struct {
+	Id         int       `orm:"pk;auto"`
+	Name       string    `orm:size(15);unique`
+	NameCn     string    `orm:size(10);null`
+	CreateTime time.Time `orm:"auto_now_add;type(datetime)"`
+	UpdateTime time.Time `orm:"auto_now;type(datetime)"`
+}
+
 func (account Account) String() string {
-	return fmt.Sprintf("\nid:%s\nusdt:%.10f\nbtc:%.10f\neth:%.10f\nbnb:%.10f\neos:%.10f\nxrp:%.10f\n[createTime:%s updateTime:%s]\n",
-		account.Id, account.Usdt, account.Btc, account.Eth, account.Bnb, account.Eos, account.Xrp, account.CreateTime, account.UpdateTime)
+	return fmt.Sprintf("\nid:%d\nplatform:%s\nusdt:%.10f\nbtc:%.10f\neth:%.10f\nbnb:%.10f\neos:%.10f\nxrp:%.10f\n[createTime:%s updateTime:%s]\n",
+		account.Id, account.Platform, account.Usdt, account.Btc, account.Eth, account.Bnb, account.Eos, account.Xrp, account.CreateTime, account.UpdateTime)
 }
 
 type Account struct {
-	Id         string
-	Usdt       float64
-	Btc        float64
-	Eth        float64
-	Bnb        float64
-	Eos        float64
-	Xrp        float64
-	CreateTime string //time.Time []uint8
-	UpdateTime string //time.Time []uint8
+	Id         int       `orm:"pk;auto"`
+	Platform   string    `orm:size(15)`
+	Usdt       float64   `orm:"digits(18);decimals(10)"`
+	Btc        float64   `orm:"digits(18);decimals(10)"`
+	Eth        float64   `orm:"digits(18);decimals(10)"`
+	Bnb        float64   `orm:"digits(18);decimals(10)"`
+	Eos        float64   `orm:"digits(18);decimals(10)"`
+	Xrp        float64   `orm:"digits(18);decimals(10)"`
+	CreateTime time.Time `orm:"auto_now_add;type(datetime)"`
+	UpdateTime time.Time `orm:"auto_now;type(datetime)"`
 }
 
 func (strategyLowBuyHighSell StrategyLowBuyHighSell) String() string {
-	return fmt.Sprintf("id:%\nsymbol:%s\nplatform:%sspend:%.10f\npositionAverage:%.10f\nlastSpend:%.10f\nprofitPoint:%.3f\ntargetSellPrice:%.10f\nbuyPoint:%.3f\ntargetBuyPrice:%.10f\nmonthAverage:%.10f[createTime:%s updateTime:%s]\n",
+	return fmt.Sprintf("id:%d\nsymbol:%s\nplatform:%sspend:%.10f\npositionAverage:%.10f\nlastSpend:%.10f\nprofitPoint:%.3f\ntargetSellPrice:%.10f\nbuyPoint:%.3f\ntargetBuyPrice:%.10f\nmonthAverage:%.10f[createTime:%s updateTime:%s]\n",
 		strategyLowBuyHighSell.Id, strategyLowBuyHighSell.Symbol, strategyLowBuyHighSell.Platform, strategyLowBuyHighSell.Spend, strategyLowBuyHighSell.PositionAverage, strategyLowBuyHighSell.LastSpend,
 		strategyLowBuyHighSell.TargetProfitPoint, strategyLowBuyHighSell.TargetSellPrice, strategyLowBuyHighSell.TargetBuyPoint, strategyLowBuyHighSell.TargetBuyPrice, strategyLowBuyHighSell.MonthAverage,
 		strategyLowBuyHighSell.CreateTime, strategyLowBuyHighSell.UpdateTime)
 }
 
 type StrategyLowBuyHighSell struct {
-	Id                int
-	Symbol            string
-	Platform          string
-	Spend             float64
-	PositionAverage   float64
-	LastSpend         float64
-	TargetProfitPoint float32
-	TargetSellPrice   float64
-	TargetBuyPoint    float32
-	TargetBuyPrice    float64
-	MonthAverage      float64
-	CreateTime        string //time.Time []uint8
-	UpdateTime        string //time.Time []uint8
+	Id                int       `orm:"pk;auto"`
+	Symbol            string    `orm:"size(15)"`
+	Platform          string    `orm:"size(15)"`
+	Spend             float64   `orm:"digits(18);decimals(10)"`
+	PositionAverage   float64   `orm:"digits(18);decimals(10)"`
+	LastSpend         float64   `orm:"digits(18);decimals(10)"`
+	TargetProfitPoint float32   `orm:"digits(4);decimals(3);default(1.025)"`
+	TargetSellPrice   float64   `orm:"digits(18);decimals(10)"`
+	TargetBuyPoint    float32   `orm:"digits(4);decimals(3);default(0.95)"`
+	TargetBuyPrice    float64   `orm:"digits(18);decimals(10)"`
+	MonthAverage      float64   `orm:"digits(18);decimals(10)"`
+	CreateTime        time.Time `orm:"auto_now_add;type(datetime)"`
+	UpdateTime        time.Time `orm:"auto_now;type(datetime)"`
 }
