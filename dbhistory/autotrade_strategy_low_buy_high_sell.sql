a2ad59fcc3fae16ba8f0461a708a0b37d6926ac0
CrazyBunQnQ <baobao222222@qq.com>
2019-07-25 12:30:10 +0800

    Fixed an error in the create table statement.

--- dbhistory/autotrade_strategy_low_buy_high_sell.sql
+++ dbhistory/autotrade_strategy_low_buy_high_sell.sql
@@ -1,1 +1,7 @@
-INSERT INTO autotrade.strategy_low_buy_high_sell (id, symbol, coin_name, platform, quantity, spend, position_average, last_spend, spend_coefficient, target_profit_point, target_sell_price, target_buy_point, target_buy_price, month_average, status, enabled, actual_cost, create_time, update_time) VALUES (1, 'XRPUSDT', 'XRP', 'binance', 574.7000000000, 189.0000000000, 0.3288672351, 120.0000000000, 2, 1.03, 0.3387332521, 0.92, 0.3025578563, 0.3123500000, 1, 1, 189.0000000000, '2019-07-24 08:09:24', '2019-07-25 00:16:52');
+INSERT INTO autotrade.strategy_low_buy_high_sell (id, symbol, coin_name, platform, quantity, spend, position_average,
+                                                  last_spend, spend_coefficient, target_profit_point, target_sell_price,
+                                                  target_buy_point, target_buy_price, month_average, last_order_id,
+                                                  enabled, actual_cost, create_time, update_time)
+VALUES (1, 'XRPUSDT', 'XRP', 'binance', 574.7000000000, 189.0000000000, 0.3288672351, 120.0000000000, 2, 1.03,
+        0.3387332521, 0.92, 0.3025578563, 0.3123500000, 0, 1, 189.0000000000, '2019-07-24 08:09:24',
+        '2019-07-25 00:16:52');
